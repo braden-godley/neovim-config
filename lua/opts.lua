744a20cf2c8487b7e8798ad24bd888eda1f3de85
Braden Godley <braden@bgodley.com>
2024-05-17 07:05:42 -0700

    usse spaces instead of tabs

--- lua/opts.lua
+++ lua/opts.lua
@@ -2,6 +2,7 @@ vim.opt.rnu = true
 --vim.opt.syntax = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.termguicolors = true
 vim.opt.clipboard = "unnamedplus"
