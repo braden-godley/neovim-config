d1ea4431f52b88891201e8619d24f0ea3bcf1868
Braden Godley <braden@bgodley.com>
2024-05-17 07:05:32 -0700

    copilot dismiss

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -38,6 +38,7 @@ vim.keymap.set('i', '<C-f>', 'copilot#Accept("\\<CR>")', {
   replace_keycodes = false
 })
 vim.g.copilot_no_tab_map = true
+vim.keymap.set('i', '<C-h>', '<Plug>(copilot-dismiss)')
 
 -- better indenting
 vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true, silent = true })
