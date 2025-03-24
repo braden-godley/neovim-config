d3fc4115208ae680e9e71c97391a3a52d9dba845
Braden Godley <braden@bgodley.com>
2025-03-24 12:49:17 -0700

    begin rewrite

---
+++ lua/config/keybinds.lua
@@ -0,0 +1,5 @@
+-- move between panes
+vim.keymap.set('n', '<C-h>', ":wincmd h<CR>", { silent = true })
+vim.keymap.set('n', '<C-j>', ":wincmd j<CR>", { silent = true })
+vim.keymap.set('n', '<C-k>', ":wincmd k<CR>", { silent = true })
+vim.keymap.set('n', '<C-l>', ":wincmd l<CR>", { silent = true })
---
+++ lua/plugins/testspec.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"folke/tokyonight.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+		-- load the colorscheme here
+		vim.cmd([[colorscheme tokyonight]])
+		end,
+	}, 
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup {}
+        end,
+    },
+    {
+        "romgrk/barbar.nvim",
+        dependencies = {
+            "lewis6991/gitsigns.nvim",
+            "nvim-tree/nvim-web-devicons",
+        }
+    }
+}
