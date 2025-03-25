40efdb799cfbfb6156239896123b4e79f124215e
Braden Godley <braden@bgodley.com>
2025-03-25 09:26:39 -0700

    lsps

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,8 +9,30 @@ return {
         config = function()
             require("mason").setup()
             require("mason-lspconfig").setup({
-                ensure_installed = { "eslint", "lua_ls", "ts_ls" }
+                ensure_installed = { 
+                    "eslint", 
+                    "lua_ls", 
+                    "ts_ls",
+                }
             })
+            require("lspconfig").lua_ls.setup {
+                settings = {
+                    Lua = {
+                        runtime = {
+                            version = 'LuaJIT',
+                        },
+                        diagnostics = {
+                            globals = { 'vim' },
+                        },
+                        telemetry = {
+                            enable = false,
+                        },
+                    },
+                },
+            }
+            require("lspconfig").ts_ls.setup {}
+            require("lspconfig").gopls.setup {}
+            require("lspconfig").eslint.setup {}
         end,
     },
     {
